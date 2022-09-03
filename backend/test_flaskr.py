0ed491cd52f0bc5e3fb45e7c0600ba50ebcd02ba
Ogirimah <93133254+Ogirimah@users.noreply.github.com>
2022-09-03 13:06:57 +0100

    Define the test functions for the defined endpoints. No implementation

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -26,13 +26,36 @@ class TriviaTestCase(unittest.TestCase):
             self.db.create_all()
     
     def tearDown(self):
-        """Executed after reach test"""
+        """Executed after each test"""
         pass
 
     """
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    
+    def test_get_all_categories(self):
+        pass
+
+    def test_paginated_question(self):
+        pass
+
+    def test_delete_question(self):
+        pass
+
+    def test_post_new_question(self):
+        pass
+
+    def test_get_question_by_search_term(self):
+        pass
+
+    def test_get_question_by_category(self):
+        pass
+
+    def test_get_question_to_play_the_quiz(self):
+        pass
+
+
 
 
 # Make the tests conveniently executable
