9dbafd7c1834fee7c0e21cde6b299ab0e2e29f2e
Ogirimah <93133254+Ogirimah@users.noreply.github.com>
2022-09-04 10:40:20 +0100

    Define test for get question and delete question

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 import json
+from urllib import request
 from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
@@ -15,7 +16,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -34,14 +35,38 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
     
+    # =================================================================================
+    # Test for all endpoints
+    # =================================================================================
     def test_get_all_categories(self):
-        pass
+        res = self.client().get('/api/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(len(data['categories']), 6)
 
     def test_paginated_question(self):
-        pass
+        res = self.client().get('/api/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['questions'])
+        self.assertEqual(len(data['categories']), 6)
+        self.assertEqual(data['total_questions'], 19)
+        self.assertTrue(data['current_category'])
 
     def test_delete_question(self):
-        pass
+        res = self.client().delete('/api/questions/5')
+        data = json.loads(res.data)
+
+        question = Question.query.filter(Question.id == 5).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['message'])
+        self.assertEqual(question, None)
 
     def test_post_new_question(self):
         pass
@@ -55,7 +80,10 @@ class TriviaTestCase(unittest.TestCase):
     def test_get_question_to_play_the_quiz(self):
         pass
 
-
+    # =================================================================================
+    # Test for all errors
+    # =================================================================================
+    
 
 
 # Make the tests conveniently executable
